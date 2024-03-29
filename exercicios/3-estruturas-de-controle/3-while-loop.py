c1aec77ceacce17b4fa5f657137b1f527c6cb25f
giorgio-simonato <161514565+giorgio-simonato@users.noreply.github.com>
2024-03-29 19:25:12 +0000

    Realização dos exercicios e termino do curso

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,14 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
-
-
+nivel_atual = 1
+nivel_final = 3
 # Crie um while loop que imprima na tela o nível atual
-
-
+while nivel_atual <= nivel_final:
+    print(f'Você avançou um nível no curso. Seu nível atual é {nivel_atual}')
+    nivel_atual += 1
+# Crie um while loop que imprima na tela o nível atual
+while nivel_atual <= nivel_final:
+    print(f'Você avançou um nível no curso. Seu nível atual é {nivel_atual}')
+    nivel_atual += 1
 # Insira "else" no while loop anterior.
+else:
+    print('Parabéns! Você concluiu o curso.')
--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -1,8 +1,24 @@
 # Criaremos um programa para substituir números por palavras em uma lista
+indice = 0
 # 1. Crie uma lista com 15 números
+lista_numeros = list(range(15,31))
+print(lista_numeros)
 # 2. Crie um for loop para percorrer todos os elementos da lista
+for numero in lista_numeros:
 # 3. Crie uma estrutura condicional para verificar cada número da lista:
-# 3.1 Caso o número seja divisível por 3, substitua-o por "Fizz"
-# 3.2 Caso o número seja divisível por 5, substitua-o por "Buzz"
-# 3.3 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
+  # 3.1 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
+  if numero % 3 == 0 and numero % 5 == 0:
+    lista_numeros[indice] = "FizzBuzz"
+      # 3.2 Caso o número seja divisível por 3, substitua-o por "Fizz"
+  elif numero % 3 == 0:
+    lista_numeros[indice] = "Fizz"
+      # 3.3 Caso o número seja divisível por 5, substitua-o por "Buzz"
+  elif numero % 5 == 0:
+    lista_numeros[indice] = "Buzz"
+         
+  else:
+    lista_numeros[indice] = numero
 
+  indice += 1
+
+print(lista_numeros)
